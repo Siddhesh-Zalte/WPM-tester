9f577007a6745a2f59e3f69f4a4067121366bb4e
Siddhesh Zalte <zaltesiddheshlsg@gmail.com>
2025-07-04 10:50:50 +0530

    Create data.js

---
+++ client/src/data.js
@@ -0,0 +1,5 @@
+export const sampleTexts = [
+  "The quick brown fox jumps over the lazy dog.",
+  "Practice makes perfect. Keep typing to improve.",
+  "Typing fast is a skill you can learn over time.",
+];
